16dd6bd61c52eab3e892643997d537edadb98eeb
Christopher Wright <chrisalanwright@gmail.com>
2025-09-18 20:44:15 -0400

    set-routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "";
+import { BrowserRouter } from "react-router";
 
 import { AuthProvider } from "./auth/AuthContext";
 import { ApiProvider } from "./api/ApiContext";
