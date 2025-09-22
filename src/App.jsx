53e057f97130abdd2eea923fad37c6573738f62a
Christopher Wright <chrisalanwright@gmail.com>
2025-09-22 16:17:04 -0400

    added-imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from "react-router";
 
 import Register from "./auth/Register";
+import Layout from "./layout/Layout.jsx";
+import ActivityDetails from "./activities/ActivityDetails";
+import RoutinesPage from "./routines/RoutinesPage";
+import RoutineDetails from "./routines/RoutineDetails";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
